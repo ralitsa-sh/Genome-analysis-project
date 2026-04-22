0af94d94ede6bbb6c066dcbd2566876f238513f9
Ralitsa Shopova <rash8155@pelle2.uppmax.uu.se>
2026-04-22 19:02:55 +0200

    Fix Canu script: add samtools module

--- code/canu_assembly_PacBio.sh
+++ code/canu_assembly_PacBio.sh
@@ -2,12 +2,13 @@
 #SBATCH -A uppmax2026-1-61
 #SBATCH -p pelle
 #SBATCH -c 4
-#SBATCH -t 02:30:00
+#SBATCH -t 01:30:00
 #SBATCH -J canu_assembly
 #SBATCH --output=%x.%j.out
 
 # Load module
-module load canu
+module load SAMtools/1.22.1-GCC-13.3.0
+module load canu/2.3-GCCcore-13.3.0-Java-17
 
 # Go to working directory
 cd /home/rash8155/Genome-analysis-project/analyses/genome_assembly_Canu
@@ -17,8 +18,7 @@ canu \
   -p efaecium \
   -d efaecium_canu_assembly \
   genomeSize=3m \
-  -pacbio \
+  -pacbio-raw \
   useGrid=false \
   maxThreads=4 \
-  maxMemory=16 \
   /home/rash8155/Genome-analysis-project/data/raw_data/genomics_data/PacBio/*.fastq.gz
